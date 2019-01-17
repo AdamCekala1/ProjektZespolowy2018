c487374705f6bdd2a1e5189e2fc5c172a3f37aad
Patryk <drab.patr@gmail.com>
2019-01-17 21:54:02 +0100

    dodanie encji

---
+++ backend/src/Entity/Category.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation as Serializer;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CategoryRepository")
+ */
+class Category
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     * @Serializer\Type("integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Serializer\Type("string")
+     */
+    private $name;
+
+//    /**
+//     * @ORM\OneToMany(targetEntity="App\Entity\Questionnaire", mappedBy="category")
+//     * @Serializer\Type("ArrayCollection<App\Entity\Questionnaire>")
+//     */
+//    private $questionnaires;
+//
+//    public function __construct()
+//    {
+//        $this->questionnaires = new ArrayCollection();
+//    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Questionnaire[]
+     */
+    public function getQuestionnaires(): Collection
+    {
+        return $this->questionnaires;
+    }
+
+    public function addQuestionnaire(Questionnaire $questionnaire): self
+    {
+        if (!$this->questionnaires->contains($questionnaire)) {
+            $this->questionnaires[] = $questionnaire;
+            $questionnaire->setCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeQuestionnaire(Questionnaire $questionnaire): self
+    {
+        if ($this->questionnaires->contains($questionnaire)) {
+            $this->questionnaires->removeElement($questionnaire);
+            // set the owning side to null (unless already changed)
+            if ($questionnaire->getCategory() === $this) {
+                $questionnaire->setCategory(null);
+            }
+        }
+
+        return $this;
+    }
+}
